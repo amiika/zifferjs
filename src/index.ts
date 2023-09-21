ad927c5522dc5270477d4466f5b98a8e39f5ebfe
Miika Alonen <amiika@gmail.com>
2023-09-21 23:34:15 +0300

    Export more methods

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { Ziffers, pattern } from './ziffers.ts';
 export { Pitch, Chord, Rest, Event } from './types.ts';
 export { choose, seededRandom } from './utils.ts';
 export { getScale, isScale, SCALES, CHORDS, CHORD_NAMES } from './defaults.ts';
-export { freqToMidi, midiToFreq, resolvePitchBend, noteFromPc, noteNameToPitchClass, chord, safeScale, getChordFromScale, getScaleNotes, noteNameToMidi } from './scale.ts';
+export { freqToMidi, midiToFreq, midiToNoteName, parseRoman, resolvePitchBend, noteFromPc, noteNameToPitchClass, chord, safeScale, getChordFromScale, chordFromDegree, getScaleNotes, noteNameToMidi, stepsToScale, scaleToSteps, voiceLead, voiceLeadChords } from './scale.ts';
 export { parse as parseScala } from './parser/scalaParser.ts';
