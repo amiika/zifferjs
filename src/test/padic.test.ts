10f5012dd56f89a4c7e879f2d57b2e9e5bb1e6eb
Miika Alonen <amiika@gmail.com>
2023-11-04 11:51:47 +0200

    Some pAdic tests

---
+++ src/test/padic.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import { pAdicExpansion, pAdicNorm, pAdicValuation } from '../patterns.ts';
+describe('padic-tests', () => {
+
+  it('padic', () => {
+    expect(pAdicValuation(1000,7)).toEqual(0);
+    expect(pAdicValuation(1002,7)).toEqual(0);
+    expect(pAdicNorm(10,11)).toEqual(1);
+    expect(pAdicExpansion(100,7,4)).toEqual([2,0,2]);
+    expect(pAdicExpansion(101,7,4)).toEqual([3,0,2]);
+    expect(pAdicExpansion(102,7,4)).toEqual([4,0,2]);
+    expect(pAdicExpansion(1000,7,4)).toEqual([6,2,6]);
+    expect(pAdicExpansion(99999,4)).toEqual([3,3,1,2,2,1,0,2,1,0]);
+  })
+
+})
