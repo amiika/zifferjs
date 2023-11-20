3fc5cae50bf2a2b99e3ec25ee50f5a5ef89083cb
Miika Alonen <amiika@gmail.com>
2023-11-21 00:27:31 +0200

    New structure for available seventh transformations

--- src/tonnetz.ts
+++ src/tonnetz.ts
@@ -1610,17 +1610,63 @@ export const getAvailableTransform = (transformations: ObjectTransformationsSeve
     return Object.keys(transformations).filter(key => key.includes(availableString));
 }
 
-export const AVAILABLESEVENTHSTRANSFORMATIONS: { readonly [key: string]: readonly string[] } = {
-    "7": ['p12', 'p14', 'r12', 'l13', 'l15', 'q15', 'qq51', 'n51', 'p18', 'p19', 'l71', 'rr19'],
-    "m7": ['p12', 'p23', 'r12', 'r23', 'r42', 'l42', 'p26', 'q62'],
-    "hdim7": ['p23', 'p35', 'r23', 'r35', 'r53', 'l13', 'q43', 'rr35', 'p39', 'r63', 'qq38'],
-    "maj7": ['p14', 'r42', 'l42', 'q43', 'p47', 'p64'],
-    "dim7": ['p35', 'r35', 'r53', 'l15', 'q15', 'rr35', 'qq51', 'n51'],
-    "minMaj7": ['p26', 'p64', 'r63', 'r76', 'r86', 'q62', 'q76'],
-    "maj7aug5": ['p47', 'p87', 'r76', 'l71', 'q76'],
-    "dom7aug5": ['p18', 'p87', 'p98', 'r86', 'l89', 'rr98', 'qq38', 'qq98'],
-    "dom7b5": ['p19', 'p39', 'p98', 'l89', 'rr19', 'rr98', 'qq98']
-};
+export const AVAILABLESEVENTHSTRANSFORMATIONS: { readonly [key: string]: { readonly [key: string]: readonly string[] } } = {
+    "7": {
+      "p": ['p12', 'p14', 'p18', 'p19'],
+      "l": ['l13', 'l15', 'l71'],
+      "r": ['r12', 'rr19'],
+      "q": ['q15', 'qq51'],
+      "n": ['n51']
+    },
+    "m7": {
+      "p": ['p12', 'p23', 'p26'],
+      "l": ['l42'],
+      "r": ['r12', 'r23', 'r42'],
+      "q": ['q62']
+    },
+    "hdim7": {
+      "p": ['p23', 'p35','p39'],
+      "l": ['l13'],
+      "r": ['r23', 'r35', 'r53', 'r63', 'rr35'],
+      "q": ['q43', 'qq38']
+    },
+    "maj7": {
+      "p": ['p14', 'p47', 'p64'],
+      "l": ['l42'],
+      "r": ['r42'],
+      "q": ['q43']
+    },
+    "dim7": {
+      "p": ['p35'],
+      "l": ['l15'],
+      "r": ['r35', 'r53'],
+      "q": ['q15', 'qq51'],
+      "n": ['n51']
+    },
+    "minMaj7": {
+      "p": ['p26', 'p64'],
+      "r": ['r63', 'r76', 'r86'],
+      "q": ['q62', 'q76']
+    },
+    "maj7aug5": {
+      "p": ['p47', 'p87'],
+      "l": ['l71'],
+      "r": ['r76'],
+      "q": ['q76']
+    },
+    "dom7aug5": {
+      "p": ['p18', 'p87', 'p98'],
+      "l": ['l89'],
+      "r": ['r86','rr98'],
+      "q": ['qq38', 'qq98']
+    },
+    "dom7b5": {
+      "p": ['p19', 'p39', 'p98'],
+      "l": ['l89'],
+      "r": ['rr19', 'rr98'],
+      "q": ['qq98']
+    }
+  };
 
 export const getAvailableSeventhsTransformations = (chord: Tetrachord, tonnetz: TonnetzSpaces = [3, 4, 5]): readonly string[] | string => {
     for (const chordFunction of Object.keys(CHORD_TYPES_SEVENTHS)) {
