858cb1415d8d64961fb09cc3f4d0820f4e57234c
Edgar Delgado Vega <edelve91@gmail.com>
2023-12-24 21:04:30 -0500

    feat(patterns): add Mordell Curves generator

--- src/patterns.ts
+++ src/patterns.ts
@@ -377,3 +377,23 @@ export function* pAdicExpansionEnneanacci(baseNumber: number, precision: number 
         [a, b, c, d, e, f, g, h, i] = [i, a, b, c, d, e, f, g, a + b + c + d + e + f + g + h + i];
     }
 }
+
+export function* mordellCurve(x: number, k: number, stepSize: number = 1): Generator<[number, number]> {
+    while (true) {
+        let y = Math.sqrt(Math.pow((x), 3) + (k))
+        if (!isNaN(y)) {
+            yield [x, y]
+        }
+        x += stepSize;
+    }
+}
+
+export function* mordellMusicPoints(x: number, k: number, modulo: number = 12, stepSize: number = 1): Generator<number> {
+    while (true) {
+        let y = Math.sqrt(Math.pow(x, 3)) + Math.abs(k)
+        if (!isNaN(y)) {
+            yield Math.round(y) % modulo
+        }
+        x += stepSize;
+    }
+}
